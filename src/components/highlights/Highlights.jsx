4f82a42754fe18ae6ec784aeffa4a6cf6d612da6
= <=>
2023-07-20 16:51:45 +0530

    commit

--- src/components/highlights/Highlights.jsx
+++ src/components/highlights/Highlights.jsx
@@ -7,35 +7,35 @@ import Image3 from "./images/3.jpg";
 import Image4 from "./images/4.jpg";
 import Image5 from "./images/5.jpg";
 
+// ... (Your imports and component definition remain the same)
+
 const Highlights = () => {
     return (
         <div className={styles.container}>
             <div className={styles.text}>
-
                 <h1>Yatra Highlights</h1>
                 <p>Join the Sacred Mandir Yatra for an Unforgettable Experience!</p>
             </div>
             <div className={styles.carousel}>
-                <div className={styles.card1}>
+                <div className={styles.card}>
                     <Image width={290} height={400} alt='image' src={Image1}  />
                 </div>
-                <div className={styles.card2}>
+                <div className={styles.card}>
                     <Image width={290} height={400} alt='image' src={Image2}  />
                 </div>
-                <div className={styles.card3}>
+                <div className={styles.card}>
                     <Image width={290} height={400} alt='image' src={Image3}  />
                 </div>
-                <div className={styles.card4}>
+                <div className={styles.card}>
                     <Image width={290} height={400} alt='image' src={Image4}  />
                 </div>
-                <div className={styles.card5}>
+                <div className={styles.card}>
                     <Image width={290} height={400} alt='image' src={Image5}  />
                 </div>
-
             </div>
-
         </div>
     )
 }
 
 export default Highlights
+
