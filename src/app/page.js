b92d0f5d50612816ca285017e0e43fa6637ac7d3
= <=>
2023-07-20 11:21:01 +0530

    This is a new commit for what I originally planned to be amended

---
+++ src/app/page.js
@@ -0,0 +1,47 @@
+import React from "react";
+import styles from "./page.module.css";
+import Link from "next/link";
+import { Cagliostro, Poppins, Russo_One, Angkor } from "next/font/google";
+
+const poppins = Poppins({
+  weight: "400",
+  subsets: ["latin"],
+});
+
+const angkor = Angkor({
+  weight: "400",
+  subsets: ["latin"],
+});
+const biorhyme = Cagliostro({
+  weight: "400",
+  subsets: ["latin"],
+});
+
+const Home = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.navbar}>
+        <div className={styles.navleft}>
+          <Link href="#" style={poppins.style}>Home</Link>
+          <Link href="#" style={poppins.style}>Highlights</Link>
+        </div>
+        <div className={styles.navmid}>
+          <Link href="#" style={angkor.style}>
+            YatraSagar
+          </Link>
+        </div>
+        <div className={styles.navright}>
+          <Link href="#" style={poppins.style}>Reviews</Link>
+          <Link href="#" style={poppins.style}>About</Link>
+        </div>
+      </div>
+      <div className={styles.title}>
+        <h1 style={biorhyme.style}>
+          Enlightened Journeys to Sacred <br/> Destinations Worldwide
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
