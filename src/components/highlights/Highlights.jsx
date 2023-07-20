947e92f8e89c0af5c33ab08b755dcf4943c61664
= <=>
2023-07-20 17:29:47 +0530

    commit

--- src/components/highlights/Highlights.jsx
+++ src/components/highlights/Highlights.jsx
@@ -7,7 +7,6 @@ import Image3 from "./images/3.jpg";
 import Image4 from "./images/4.jpg";
 import Image5 from "./images/5.jpg";
 
-// ... (Your imports and component definition remain the same)
 
 const Highlights = () => {
     return (
@@ -17,20 +16,20 @@ const Highlights = () => {
                 <p>Join the Sacred Mandir Yatra for an Unforgettable Experience!</p>
             </div>
             <div className={styles.carousel}>
-                <div className={styles.card}>
-                    <Image width={290} height={400} alt='image' src={Image1}  />
+                <div className={styles.card1}>
+                    <Image width={260} height={400} alt='image' src={Image1}  />
                 </div>
-                <div className={styles.card}>
-                    <Image width={290} height={400} alt='image' src={Image2}  />
+                <div className={styles.card2}>
+                    <Image width={260} height={400} alt='image' src={Image2}  />
                 </div>
-                <div className={styles.card}>
-                    <Image width={290} height={400} alt='image' src={Image3}  />
+                <div className={styles.card3}>
+                    <Image width={260} height={400} alt='image' src={Image3}  />
                 </div>
-                <div className={styles.card}>
-                    <Image width={290} height={400} alt='image' src={Image4}  />
+                <div className={styles.card4}>
+                    <Image width={260} height={400} alt='image' src={Image4}  />
                 </div>
-                <div className={styles.card}>
-                    <Image width={290} height={400} alt='image' src={Image5}  />
+                <div className={styles.card5}>
+                    <Image width={260} height={400} alt='image' src={Image5}  />
                 </div>
             </div>
         </div>
