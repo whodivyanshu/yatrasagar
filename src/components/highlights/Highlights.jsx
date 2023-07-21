b79c00689bf1945e2e90966ed4faba3f5a2657e0
= <=>
2023-07-21 09:54:03 +0530

    commit by divyanshu

--- src/components/highlights/Highlights.jsx
+++ src/components/highlights/Highlights.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+"use client"
+import React, { useEffect } from 'react';
 import styles from "./highlight.module.css";
 import Image from 'next/image';
 import Image1 from "./images/1.jpg";
@@ -6,35 +7,46 @@ import Image2 from "./images/2.jpg";
 import Image3 from "./images/3.jpg";
 import Image4 from "./images/4.jpg";
 import Image5 from "./images/5.jpg";
+import { Splide } from '@splidejs/splide';
+import '@splidejs/splide/css';
 
+import '@splidejs/splide/css/skyblue';
+import '@splidejs/splide/css/sea-green';
+
+import '@splidejs/splide/css/core';
 
 const Highlights = () => {
+    useEffect(() => {
+        const splide = new Splide('.splide', {
+            type: 'loop',
+            perPage: 5,
+            focus: 'center',
+//             type  : 'fade',
+//   rewind: true,
+        });
+
+        splide.mount();
+    }, []);
+
     return (
         <div className={styles.container}>
             <div className={styles.text}>
                 <h1>Yatra Highlights</h1>
                 <p>Join the Sacred Mandir Yatra for an Unforgettable Experience!</p>
             </div>
-            <div className={styles.carousel}>
-                <div className={styles.card1}>
-                    <Image width={260} height={400} alt='image' src={Image1}  />
+            <section className="splide" aria-label="Splide Basic HTML Example">
+                <div className="splide__track">
+                    <ul className="splide__list">
+                        <li className="splide__slide slide1"><Image alt='hey' width={250} height={400} src={Image1} /></li>
+                        <li className="splide__slide slide2"><Image alt='hey' width={250} height={400} src={Image2} /></li>
+                        <li className="splide__slide slide3"><Image alt='hey' width={250} height={400} src={Image3} /></li>
+                        <li className="splide__slide slide4"><Image alt='hey' width={250} height={400} src={Image4} /></li>
+                        <li className="splide__slide slide5"><Image alt='hey' width={250} height={400} src={Image5} /></li>
+                    </ul>
                 </div>
-                <div className={styles.card2}>
-                    <Image width={260} height={400} alt='image' src={Image2}  />
-                </div>
-                <div className={styles.card3}>
-                    <Image width={260} height={400} alt='image' src={Image3}  />
-                </div>
-                <div className={styles.card4}>
-                    <Image width={260} height={400} alt='image' src={Image4}  />
-                </div>
-                <div className={styles.card5}>
-                    <Image width={260} height={400} alt='image' src={Image5}  />
-                </div>
-            </div>
+            </section>
         </div>
     )
 }
 
-export default Highlights
-
+export default Highlights;
